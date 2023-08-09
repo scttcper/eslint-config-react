feffa7d7c9dee5a00b246161950752a70aaf19cb
Scott Cooper <scttcper@gmail.com>
2023-08-08 21:26:42 -0700

    feat: disable in scope and readonly props

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 module.exports = {
-  extends: [
-    'xo-react/space',
-    '@ctrl/eslint-config',
-  ],
+  extends: ['xo-react/space', '@ctrl/eslint-config'],
   rules: {
     'react/no-unescaped-entities': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prefer-read-only-props': 'off',
   },
 };
